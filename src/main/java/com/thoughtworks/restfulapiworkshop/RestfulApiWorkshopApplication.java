7493517f893c96402ee18a01482ee6874bdb3b9c
Caius Eugene NHS <caius_hns@Caiuss-MacBook-Pro.local>
2021-02-23 09:19:46 +0000

    initialized project

---
+++ src/main/java/com/thoughtworks/restfulapiworkshop/RestfulApiWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.restfulapiworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/restfulapiworkshop/RestfulApiWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.restfulapiworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
